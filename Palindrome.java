8e5378621274756805eb10513c15bacc5c6770f0
meryam1a11 <>
2021-04-07 21:59:28 +0000

    done

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,14 @@
 public class Palindrome{
     public static boolean is_palindrome(String s)
     {
+       boolean is_palindrome=false;
+        int i;
+        for(i=0;i<=s.length()-1;i++){
+          if(s.charAt(i)==s.charAt(s.length()-1-i)){
+            return true;
+          }
+        }
+        return false;
         // add your code here
-        return true;
     }
 }
